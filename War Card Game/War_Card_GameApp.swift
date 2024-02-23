72b3d1f33e82394883f922d9b83f12727183fee6
andrewlin11 <andrewhlin5@gmail.com>
2024-02-23 09:33:47 -0600

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Andrew Lin on 2/19/24.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
